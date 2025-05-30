c91d534db990aed23d3a3bded39b352f21a006fa
4yd3n1 <4yd3n1@github.com>
2025-05-30 14:09:48 +0200

    Deploy new MEGA contract 0xD0A3275dDe0acAba2085C3f58dCef830a89b2BB9

--- contracts/mega.ts
+++ contracts/mega.ts
@@ -1,4 +1,4 @@
 import MEGA_ABI from './MEGA_ABI.json';
 
 export { MEGA_ABI };
-export const MEGA_CONTRACT_ADDRESS = '0x79b22FE072dcDCa91fEE4dDc2CaeCbfb155cCcaA';
+export const MEGA_CONTRACT_ADDRESS = '0xD0A3275dDe0acAba2085C3f58dCef830a89b2BB9';
