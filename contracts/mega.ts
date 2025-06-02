db1857663195cdd7c82e86482456310bd36466aa
4yd3n1 <4yd3n1@github.com>
2025-06-02 17:00:00 +0200

    Update contract ABI and address: Custom errors optimization, 7-day duration,  target

--- contracts/mega.ts
+++ contracts/mega.ts
@@ -1,13 +1,13 @@
 import MEGA_ABI from './MEGA_ABI.json';
 
 export { MEGA_ABI };
-export const MEGA_CONTRACT_ADDRESS = '0x03E5077C9cEF73E6E6E7586562c76b198dED3EdF';
+export const MEGA_CONTRACT_ADDRESS = '0xac3279679a7b7309C0F4D6C52d0bA801BD14AF62';
 
 // Contract deployment info (auto-generated)
 export const DEPLOYMENT_INFO = {
-  address: '0x03E5077C9cEF73E6E6E7586562c76b198dED3EdF',
+  address: '0xac3279679a7b7309C0F4D6C52d0bA801BD14AF62',
   chainId: 11155111, // Sepolia
-  deployedAt: '2025-06-02T13:07:12.482Z',
+  deployedAt: '2025-06-02T14:30:39.167Z',
   deployer: '0x1113C54cdb2EF5b41D6f48280CEA00205415E6b3'
 };
 
