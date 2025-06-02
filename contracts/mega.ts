74507875f4a427a59bfe999dc598d8fb670f03a4
4yd3n1 <4yd3n1@github.com>
2025-06-02 15:07:38 +0200

    Deploy new MEGA contract 0x03E5077C9cEF73E6E6E7586562c76b198dED3EdF

--- contracts/mega.ts
+++ contracts/mega.ts
@@ -1,13 +1,13 @@
 import MEGA_ABI from './MEGA_ABI.json';
 
 export { MEGA_ABI };
-export const MEGA_CONTRACT_ADDRESS = '0x585BEe523fC79ED65A4Ffeeda318D4D7e1fFCD5f';
+export const MEGA_CONTRACT_ADDRESS = '0x03E5077C9cEF73E6E6E7586562c76b198dED3EdF';
 
 // Contract deployment info (auto-generated)
 export const DEPLOYMENT_INFO = {
-  address: '0x585BEe523fC79ED65A4Ffeeda318D4D7e1fFCD5f',
+  address: '0x03E5077C9cEF73E6E6E7586562c76b198dED3EdF',
   chainId: 11155111, // Sepolia
-  deployedAt: '2025-06-02T12:31:27.107Z',
+  deployedAt: '2025-06-02T13:07:12.482Z',
   deployer: '0x1113C54cdb2EF5b41D6f48280CEA00205415E6b3'
 };
 
